7cfd6a188c5723dcd11a51c1836beb912a0406d0
Coli <awestlifeblue@gmail.com>
2021-11-08 17:03:52 +0800

    Async Patterns

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-// npm - global command, comes with node
-// npm --version
-
-
-// local dependency - use it only in this particular PromiseRejectionEvent
-// npm i <packageName>
-
-
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (mac)
-
-
-//package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the rootCertificates, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+const {readFile} = require('fs');
 
 
 
+const getText = (path) => {
+  return new Promise((resolve,reject) => {  
+    readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
 
-const _ = require('lodash');
+getText('../content/first.txt')
+  .then(result => console.log(result))
+  .catch(err => console.log(err))
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems);
 
 
